6bab97fd392b660e76b294f820aedc6d114512d6
jcortes18 <jcortes@oakland.edu>
2021-10-24 23:14:45 -0400

    Created a bottom navigation bar
    Bottom navigation bar stays static throughout all pages.
    Navigation bar includes 3 buttons (home, marketplace, and profile)
    Once clicked, application navigates to specific page.

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+import 'homescreen.dart';
+import 'marketplace_main.dart';
+import 'profile.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Griz Connect',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedPage = 0;
+
+  final _pageOptions = [
+    Homescreen(),
+    MarketplaceTab(),
+    ProfileTab()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _pageOptions[selectedPage],
+        bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: DotNavigationBar(
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          backgroundColor: Colors.black,
+          dotIndicatorColor: Colors.white,
+          unselectedItemColor: Colors.grey[300],
+          selectedItemColor: Colors.blue,
+          items: [
+            DotNavigationBarItem(
+                icon: Icon(Icons.home),
+            ),
+            DotNavigationBarItem(
+                icon: Icon(Icons.sell),
+            ),
+            DotNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+            ),
+          ],
+
+          currentIndex: selectedPage,
+          onTap: (index){
+            setState(() {
+              selectedPage = index;
+            });
+          },
+        )
+        )
+    );
+  }
+}
--- lib/profile.dart
+++ lib/profile.dart
@@ -2,16 +2,16 @@ import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 
-class MarketplaceTab extends StatelessWidget {
+class ProfileTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'GrizzConnect Marketplace',
+      title: 'GrizzConnect Profile',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.green,
       ),
-      home: const Center(child: Text('marketplace')),
+      home: const Center(child: Text('profile')),
     );
   }
 }
