7281b514880eb64e40bb7573124130c3352b1147
jcortes18 <jcortes@oakland.edu>
2021-11-13 14:20:28 -0500

    Connected to firebase db

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
